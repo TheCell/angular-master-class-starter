2b78cd58eca07a8be30d2b2b3029d98a356cd54d
Simon <simon_hischier@hotmail.com>
2020-10-05 16:14:24 +0200

    added wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: ContactsListComponent },
-  { path: 'contact/:id', component: ContactsDetailComponent }
+  { path: 'contact/:id', component: ContactsDetailComponent },
+  { path: '**', redirectTo: '/' }
 ]
