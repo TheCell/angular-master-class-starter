0059f9676d64fa3c05be1e0398f762c4462b0123
Simon <simon_hischier@hotmail.com>
2020-10-05 14:51:27 +0200

    Dependency Injection

---
+++ src/app/contacts.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { CONTACT_DATA } from './data/contact-data';
+import { Contact } from './models/contact';
+
+@Injectable()
+export class ContactsService {
+  private contacts: Contact[] = CONTACT_DATA;
+
+  public getContacts(): Contact[] {
+    return this.contacts;
+  }
+}
