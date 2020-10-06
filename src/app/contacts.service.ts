b16bad53b8d991cd77b72c77c0d97026eb7a6127
Simon <simon_hischier@hotmail.com>
2020-10-06 11:07:30 +0200

    specified return, fixed potential error

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Contact } from './models/contact';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 interface ContactResponse { item: Contact }
 interface ContactsResponse { items: Contact[] }
@@ -12,20 +13,20 @@ export class ContactsService {
 
   constructor(private http: HttpClient) {}
 
-  public getContact(id: string) {
+  public getContact(id: string): Observable<Contact> {
     let url = `${this.API_ENDPOINT}/contacts/${id}`;
     return this.http.get<ContactResponse>(url)
       .pipe(map((data) => data.item));
   }
 
-  public getContacts() {
+  public getContacts(): Observable<Contact[]> {
     let url = `${this.API_ENDPOINT}/contacts`;
     return this.http.get<ContactsResponse>(url)
       .pipe(map((data) => data.items));
   }
 
-  public updateContact(contact: Contact) {
+  public updateContact(contact: Contact): Observable<Contact> {
     let url = `${this.API_ENDPOINT}/contacts/${contact.id}`;
-    return this.http.put(url, contact)
+    return this.http.put<ContactResponse>(url, contact).pipe(map(data => data.item));
   }
 }
